ac801c2e261abd80d66d0e08b874ad51ccf5034d
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-12-20 01:19:29 -0500

    retrieved comics from API call

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,16 @@
 
 const key = `ts=8&apikey=1f2c8ca0a08a1f8caa7f0cab071dca30&hash=09ef186eb114f844ac8698e059ea9181`;
 
-let latestComicsurl = `https://gateway.marvel.com/v1/public/comics?${key}&format=comic&formatType=comic&noVariants=true&dateDescriptor=thisWeek&orderBy=-onsaleDate&limit=5`
+let latestComicsurl = `https://gateway.marvel.com/v1/public/comics?${key}&format=comic&formatType=comic&noVariants=true&dateDescriptor=thisWeek&orderBy=-onsaleDate&limit=3`
 
+function lastestComics(url) {
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let comics = data.data.results;
+    })
+
+}
+
+lastestComics(latestComicsurl);
