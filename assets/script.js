3c460895c8b529e0c7277218aa8e2d89a693f61c
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-12-20 01:00:41 -0500

    Created Key for API call

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+
+const key = `ts=8&apikey=1f2c8ca0a08a1f8caa7f0cab071dca30&hash=09ef186eb114f844ac8698e059ea9181`;
+
+let latestComicsurl = `https://gateway.marvel.com/v1/public/comics?${key}&format=comic&formatType=comic&noVariants=true&dateDescriptor=thisWeek&orderBy=-onsaleDate&limit=5`
+
